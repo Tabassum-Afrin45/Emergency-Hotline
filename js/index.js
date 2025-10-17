01bd86510369636815796ec3231f3ba7d7abf51b
Tabassum-Afrin45 <tabassumafrin875@gmail.com>
2025-10-17 17:24:34 +0600

    Added Call button Features

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Heart icon Feature
 let count=0
 const heartIcons=document.getElementsByClassName("heart-icon")
 const availableHearts=document.getElementById("available-hearts")
@@ -7,4 +8,36 @@ for(const heartIcon of heartIcons){
   count++;
   availableHearts.innerText=count;
 })
+}
+
+// Call Button Feature
+let coins=parseInt(document.getElementById("available-coins").innerText)
+const callButtons=document.getElementsByClassName("call-button")
+const availableCoins=document.getElementById("available-coins")
+for(const callButton of callButtons){
+  callButton.addEventListener("click",function(){
+      const serviceName=callButton.parentNode.parentNode.children[1].innerText
+      const serviceNumber=callButton.parentNode.parentNode.children[3].innerText
+      if(coins<20){
+        alert("You do not have sufficient coins!")
+        return;
+      }
+      else{
+        alert("Calling "+serviceName+" "+serviceNumber+"...")
+        coins-=20;
+        availableCoins.innerText=coins;
+       
+        // History Feature
+        const parentContainer=document.getElementById("callHistory-Container")
+        const newDiv=document.createElement("div")
+        newDiv.innerHTML=`        <div class="flex justify-between bg-[#fafafa] p-4 rounded-lg mx-5 mb-5">
+          <div>
+            <h3 class="font-semibold">${serviceName}</h3>
+             <p class="text-gray-600">${serviceNumber}</p>
+          </div>
+          <p class="my-auto ">1:36:58 AM</p>
+        </div>`
+        parentContainer.appendChild(newDiv);
+      }
+  })
 }
